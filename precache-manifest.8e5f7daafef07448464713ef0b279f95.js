2f7cc4309773ba7801e61798fe32768e37089c62
Mark <hisshash@gmail.com>
2019-11-11 20:15:48 -0500

    Updates

---
+++ precache-manifest.8e5f7daafef07448464713ef0b279f95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a28b505a3b0cc614360d7e5db0e53935",
+    "url": "/polished-shades/index.html"
+  },
+  {
+    "revision": "5998c20b5096063a6ef7",
+    "url": "/polished-shades/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3c9498f785512f999d52",
+    "url": "/polished-shades/static/js/2.83e70124.chunk.js"
+  },
+  {
+    "revision": "5998c20b5096063a6ef7",
+    "url": "/polished-shades/static/js/main.7870468e.chunk.js"
+  },
+  {
+    "revision": "c82eb0ba5da1f6a6fbde",
+    "url": "/polished-shades/static/js/runtime-main.bce70b4b.js"
+  }
+]);
